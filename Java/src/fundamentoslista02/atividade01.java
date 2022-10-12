5ec459e48c6703cfec84cf6ed563cd6451d6e74f
VictorMatheus11 <80292871+VictorMatheus11@users.noreply.github.com>
2022-10-11 21:08:14 -0300

    atividade01

--- Java/src/fundamentoslista02/atividade01.java
+++ Java/src/fundamentoslista02/atividade01.java
@@ -4,11 +4,29 @@
  * and open the template in the editor.
  */
 package fundamentoslista02;
-
+import java.util.Scanner;
 /**
  *
  * @author aula
  */
 public class atividade01 {
-    
+    public static void main(String[] args) {
+        Scanner t= new Scanner (System.in);
+        double x;
+        double z;
+        System.out.println("digite o primeiro numero");
+        x = t.nextDouble();
+        System.out.println("digite o primeiro segundo");
+        z = t.nextDouble();
+        
+        if (x==z) {
+            System.out.println("Os numeros são iguais");
+        }
+        else {
+            System.out.println("Os numeros são diferentes");
+        }
+             
+        
+        t.close(); 
+    }
 }
