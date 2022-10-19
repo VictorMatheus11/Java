e70f36eda4d4086557756dd6a45732e969699fe7
VictorMatheus11 <80292871+VictorMatheus11@users.noreply.github.com>
2022-10-18 22:03:39 -0300

    Atividade03

---
+++ Java/src/Lista03/Atividade02.java
@@ -0,0 +1,18 @@
+/**Entrar com 20 números e imprimir a soma desses números.
+ */
+package Lista03;
+
+import java.util.Scanner;
+import jdk.nashorn.internal.ir.Symbol;
+
+/**
+ *
+ * @author aula
+ */
+public class Atividade02 {
+    public static void main(String[] args) {
+        int numero;
+        Scanner teclado = new Scanner (System.in);
+        
+    }
+}
