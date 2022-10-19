16fde492e2291a86899be31341639c57a6f14861
VictorMatheus11 <80292871+VictorMatheus11@users.noreply.github.com>
2022-10-19 17:45:06 -0300

    lista03atividade03

---
+++ Java/src/Lista03/Atividade03.java
@@ -0,0 +1,41 @@
+/*
+ * -  Entrar com 20 números e imprimir a soma dos positivos e o total de números negativos
+ *
+ */
+package Lista03;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mteixeira
+ */
+public class Atividade03 {
+    
+    public static void main(String[] args) {
+        
+        int positivo;
+        int negativo;
+        int num;
+        int cont = 1;
+        
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Digite o 1° número");
+                
+        num = teclado.nextInt();
+                
+        while (cont <=20){
+        
+            
+            cont++;
+        
+        }
+        teclado.close();
+        
+        
+        
+        
+        
+    }
+    
+}
