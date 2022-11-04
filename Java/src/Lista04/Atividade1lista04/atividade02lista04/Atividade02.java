f55816db9e97794d744571a740efc62d1c4bcdbf
VictorMatheus11 <80292871+VictorMatheus11@users.noreply.github.com>
2022-11-04 13:01:47 -0300

    Create Atividade02.java

---
+++ Java/src/Lista04/Atividade1lista04/atividade02lista04/Atividade02.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lista04.Atividade1lista04.atividade02lista04;
+
+/**
+ *
+ * @author mteixeira
+ */
+public class Atividade02 {
+    
+}
